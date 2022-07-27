03bdabec8ec546da73f23415ec76bca825d332be
Furkan Yurdakul <furkan.yurdakul@hotmail.com>
2022-07-27 21:30:50 +0300

    Create navigation component and base layouts

--- app/src/main/java/com/scorp/casestudy/furkanyurdakul/ui/MainActivity.kt
+++ app/src/main/java/com/scorp/casestudy/furkanyurdakul/ui/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.scorp.casestudy.furkanyurdakul.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity: AppCompatActivity()
 {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+    }
 }
